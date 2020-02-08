804988742215579c00bdeac4595a37351585a193
Modern Viking <thomascustoms715@gmail.com>
2020-02-08 14:00:29 -0700

    html generation works

--- app.js
+++ app.js
@@ -1,30 +1,140 @@
 const inquirer = require("inquirer");
-const Manager = require("./lib/Manager.js");
-const Engineer = require("./lib/Engineer.js");
-const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/manager.js");
+const Engineer = require("./lib/engineer.js");
+const Intern = require("./lib/intern.js");
+const makeHTML = require("./templates/mainHTML.js");
 const fs = require("fs");
+const teamArray = [];
+let teamCardsHtml = "";
 
 init();
 
 async function init() {
-  try {
-    let empSpecificData;
-    let { name } = await promptName();
-    name = capWords(name);
-    let { id } = await promptId();
-    let { email } = await promptEmail();
-    let { role } = await promptRole();
-    switch (role) {
-      case "Manager":
-        empSpecificData = await promptOfficeNum();
-        let manager = new Manager(name, id, email, empSpecificData);
-        return manager;
-      case "Engineer":
-        empSpecificData = await promptGithub();
-        let engineer = new Engineer(name, id, email, empSpecificData);
-        return engineer;
-      case "Intern":
-        empSpecificData = await promptSchool();
-        let intern = new Intern(name, id, email, empSpecificData);
-        return intern;
+  let addAnother = "Yes.";
+  do{
+    try {
+      let empSpecificData;
+      let { name } = await namePrompt();
+      name = capitalizeName(name);
+      let { id } = await idPrompt();
+      let { email } = await emailPrompt();
+      let { role } = await rolePrompt();
+      switch (role) {
+        case "Manager":
+          empSpecificData = await getOfficeNumber();
+          let manager = new Manager(name, id, email, empSpecificData.officeNumber);
+          teamArray.push(manager);
+          break;
+        case "Engineer":
+          empSpecificData = await getGithubUser();
+          let engineer = new Engineer(name, id, email, empSpecificData.username);
+          teamArray.push(engineer);
+          break;
+        case "Intern":
+          empSpecificData = await getSchool();
+          let intern = new Intern(name, id, email, empSpecificData.school);
+          teamArray.push(intern);
+          break;
+      }
+      addAnother = await addEmployees();
+    } 
+    catch (err) {
+      console.log(err);
     }
+  } 
+  while(addAnother.result === "Yes.");
+  // HTML GEN
+    for (var i=0; i<teamArray.length; i++) {
+      let card = makeHTML.makeCards(teamArray[i]);
+      teamCardsHtml += card;
+      let HTML = makeHTML.makeHTML(card);
+
+      fs.writeFile('./output/index.html', HTML, function(err) {
+        if (err) console.log('failed');
+        console.log('Success');
+    })}
+};
+
+
+function namePrompt() {
+  const name = inquirer.prompt({
+    type: "input",
+    message: "What is the new Employee's first and last name?",
+    name: "name"
+  });
+  return name;
+}
+
+function idPrompt() {
+  const id = inquirer.prompt({
+    type: "input",
+    message: "What is the new Employee's designated ID number?",
+    name: "id"
+  });
+  return id;
+}
+
+function emailPrompt() {
+  const email = inquirer.prompt({
+    type: "input",
+    message: "What is the new Employee's company email?",
+    name: "email"
+  });
+  return email;
+}
+
+function rolePrompt() {
+  const role = inquirer.prompt({
+    type: "list",
+    message: "What is the Employee's role?",
+    name: "role",
+    choices: ["Manager", "Engineer", "Intern"]
+  });
+  return role;
+}
+
+function getOfficeNumber() {
+  const officeNumber = inquirer.prompt({
+    type: "input",
+    message: "What is the Manager's office number?",
+    name: "officeNumber"
+  });
+  return officeNumber;
+}
+
+function getGithubUser() {
+    const username = inquirer.prompt({
+        type: "input",
+        message: "What is the Engineer's GitHub username?",
+        name: "username"
+      });
+      return username;
+    }
+
+function getSchool() {
+    const school = inquirer.prompt({
+        type: "input",
+        message: "What is the Intern's school?",
+        name: "school"
+      });
+      return school;
+}
+
+function addEmployees() {
+  const result = inquirer.prompt({
+    type: 'list',
+    name: 'result',
+    message: 'Add another Employee?',
+    choices: ["Yes.", "No."]
+  });
+  return result;
+}
+
+function capitalizeName(str) {
+  var splitStr = str.toLowerCase().split(" ");
+  for (var i = 0; i < splitStr.length; i++) {
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  return splitStr.join(" ");
+};
---
+++ templates/mainHTML.js
@@ -0,0 +1,83 @@
+  
+function makeCards(teamMember) {
+    let employeeData;
+    switch(teamMember.getRole()) {
+        case "Manager":
+            employeeData = `Office Number: ${teamMember.getOfficeNumber()}`;
+            break;
+        case "Engineer":
+            employeeData = `GitHub Username: ${teamMember.getGithub()}`;
+            break;
+        case "Intern":
+            employeeData = `School: ${teamMember.getSchool()}`;
+            break;
+        default:
+            console.log("No Role Found, Try Again");
+    }
+    return `   <div class="card">
+    <div class="card-content">
+        <p class="title">
+            ${teamMember.getName()}
+        </p>
+        <p class="subtitle">
+            ${teamMember.getRole()}
+        </p>
+    </div>
+    <footer class="card-footer">
+        <p class="card-footer-item">
+            <span>
+            Employee ID: ${teamMember.getId()}
+            </span>
+        </p>
+        <p class="card-footer-item">
+            <span>
+            Email: ${teamMember.getEmail()}
+            </span>
+        </p>
+        <p class="card-footer-item">
+            <span>
+            ${employeeData}
+            </span>
+        </p>
+    </footer>
+</div>`
+}
+
+
+function makeHtml(cards) {
+
+return`<!doctype html>
+<html lang="en">
+  <head>
+    <title>Team Viewer</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bulma meta -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <body>
+  <section class="hero is-info text-is-centered">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+          There Is No I In Team
+      </h1>
+      <h2 class="subtitle">
+        Here is your team
+      </h2>
+    </div>
+  </div>
+</section> 
+      <div class="container">
+          
+      <div class="columns">
+      ${cards}
+      </div>
+  </body>
+</html>`
+}
+module.exports = {
+    makeHTML: makeHtml,
+    makeCards: makeCards
+};
